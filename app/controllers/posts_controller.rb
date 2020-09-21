5c4dd5d42ffb626c799ca5a6c2c7b843ef832ff1
klaudiusz.fraj@gmail.com <klaudiusz.fraj@gmail.com>
2020-09-21 09:39:43 +0200

    add from

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
     def index 
         
     end
+    def def new
+        # @ = .new
+    end
+    def create
+        # render plain: params[:post].inspect
+    end
+    
+    
 end
